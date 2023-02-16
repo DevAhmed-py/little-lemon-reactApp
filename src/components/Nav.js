41bd2c2993847640d7af3de9deaa03b42cd50309
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2023-02-16 14:33:30 +0100

    added components

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#"> home </a></li>
+                <li><a href="#"> about </a></li>
+                <li><a href="#"> contact </a></li>
+                <li><a href="#"> social media </a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
