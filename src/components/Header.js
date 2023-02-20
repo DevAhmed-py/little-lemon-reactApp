5c2aab038643ba5d2e7b1d2a1495bf54fb2990b4
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2023-02-20 21:32:10 +0100

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,18 @@ const Header = () => {
         <div className="header">
             <Nav />
             <section className="header-section">
-                <div className="left">
+                <div className="left side">
                     <h1> Little Lemon </h1>
                     <h3> Chicago </h3>
                     <p> 
                         We are a family owned Mediterranean restaurant, 
                         focused on traditional recipes served with a 
-                        modern twist 
+                        modern twist.
                     </p>
                     <button> Reserve a Table </button>
                 </div>
-                <div className="right">
-                    <img src="" alt="header content food image" />
+                <div className="right side">
+                    <img src="images/restaurant-food.jpg" alt="header content food image" />
                 </div>
             </section>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ const Nav = () => {
     return (
         <nav>
             <div className="logo">
-                <img src="" alt="logo" />
+                <img src='images/logo.svg' alt="logo" />
             </div>
-            <ul>
+            <ul className="header-links">
                 <li><a href="#"> Home </a></li>
                 <li><a href="#"> About </a></li>
                 <li><a href="#"> Menu </a></li>
